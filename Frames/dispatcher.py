74b6b190dc50627115397695931fa998d5e870d3
Thomas Peschlow <thomas.p24@live.com>
2024-02-24 04:07:39 -0500

    Relayed data is vectorized, and can be culled

--- Frames/dispatcher.py
+++ Frames/dispatcher.py
@@ -46,7 +46,7 @@ class DispatcherFrame(Frame):
 
     def add_client(self):
         try:
-            self.relay.add_client(ip=self.out_ip.get(), port=int(self.out_port.get()))
+            self.relay.add_client(ip=self.out_ip.get(), port=int(self.out_port.get()), preset=self.out_preset.get())
         except Exception as e:
             self.msg_label.configure(text=e)
         self.draw()
@@ -72,37 +72,52 @@ class DispatcherFrame(Frame):
         Label(self, text='    ', bg='#FFFFFF').grid(row=row, column=0)
         row+=1
 
-        Label(self, text='In Port:', bg='#FFFFFF').grid(row=row, column=1, sticky='nsw')
-        self.in_port = Entry(self, bd=1)
-        self.in_port.grid(row=row, column=1, sticky='nes')
-        self.in_port.insert(0, '12345')
         self.dispatch_button = Button(self, text='Dispatch', command=self.dispatch, bg='#A6B6F7', padx=10)
-        self.dispatch_button.grid(row=row, column=2, columnspan=3, sticky='nesw')
+        self.dispatch_button.grid(row=row, column=1, columnspan=4, sticky='nesw')
+        row+=1
+
+        Label(self, text='In Port:', bg='#FFFFFF').grid(row=row, column=1, sticky='nesw')
+        self.in_port = Entry(self, bd=1)
+        self.in_port.grid(row=row, column=2, sticky='nesw')
+        self.in_port.insert(0, '12345')
+        Label(self, text='Frequency:', bg='#FFFFFF').grid(row=row, column=3, sticky='nesw')
+        self.frequency = Entry(self, bd=1)
+        self.frequency.grid(row=row, column=4, sticky='nesw')
+        self.frequency.insert(0, '10')
         row+=1
 
         Label(self, text='    ', bg='#FFFFFF').grid(row=row, column=0)
         row+=1
 
-        Label(self, text='Add Relay', bg='#FFFFFF').grid(row=row, column=1, columnspan=1, sticky='nesw')
+        Label(self, text='Add relay addresses below', bg='#FFFFFF').grid(row=row, column=1, columnspan=1, sticky='nesw')
         row+=1
-        Label(self, text='Relayed IP:', bg='#FFFFFF').grid(row=row, column=1, sticky='nsw')
+        Label(self, text='Relayed IP:', bg='#FFFFFF').grid(row=row, column=1, sticky='nesw')
         self.out_ip = Entry(self, bd=1)
-        self.out_ip.grid(row=row, column=1, columnspan=2, sticky='nes')
-        Label(self, text='Port:', bg='#FFFFFF').grid(row=row, column=3, sticky='nsw')
+        self.out_ip.grid(row=row, column=2, sticky='nesw')
+        Label(self, text='Port:', bg='#FFFFFF').grid(row=row, column=3, sticky='nesw')
         self.out_port = Entry(self, bd=1)
-        self.out_port.grid(row=row, column=3, sticky='nes')
-        Button(self, text='Add', command=self.add_client).grid(row=row, column=4, sticky='nesw')
+        self.out_port.grid(row=row, column=4, sticky='nesw')
+        row+=1
+
+        Label(self, text='Use Data:', bg='#FFFFFF').grid(row=row, column=1, sticky='nesw')
+        self.out_preset = ttk.Combobox(self, values=OSCRelay.get_presets(), state='readonly')
+        self.out_preset.grid(row=row, column=2, sticky='nesw')
+        self.out_preset.current(0)
+        Button(self, text='Add Relay', command=self.add_client).grid(row=row, column=4, sticky='nesw')
         row+=1
 
         Label(self, text='    ', bg='#FFFFFF').grid(row=row, column=0)
         row+=1
 
-        Label(self, text='Relay Addresses', bg='#FFFFFF').grid(row=row, column=1, columnspan=1, sticky='nesw')
+        Label(self, text='Current Relay Addresses', bg='#FFFFFF').grid(row=row, column=1, columnspan=1, sticky='nesw')
         row+=1
         self.clientlist = Frame(self, background='#FFFFFF', relief='solid', borderwidth=2, height=40)
         self.clientlist.grid(row=row, column=1, columnspan=4, sticky='nesw', ipadx=2, ipady=2)
         Label(self.clientlist, text='', bg='#FFFFFF').grid(row=0, column=0, columnspan=2, sticky='nes')
 
         for i, client in enumerate(self.relay.get_clients()):
-            Label(self.clientlist, text=f'Ip {client[0]} @ port {client[1]}', bg='#FFFFFF').grid(row=i//3+1, column=i%3*3, columnspan=2, sticky='nes')
-            Button(self.clientlist, text='-', command=lambda index=i: self.remove_client(index)).grid(row=i//3+1, column=i%3*3+2, sticky='nes')
+            Label(self.clientlist, text=f'Ip {client[0]} @ port {client[1]}, {client[2]}', bg='#FFFFFF', pady=2).grid(row=i//2, column=i%2*3, columnspan=2, sticky='nes')
+            Button(self.clientlist, text='-', command=lambda index=i: self.remove_client(index)).grid(row=i//2, column=i%2*3+2, sticky='nes')
+        row+=1
+        Label(self, text='Input EmotiBit messages will be vectorized, and relevant data will be sent to address.', bg='#FFFFFF').grid(row=row, column=1, columnspan=4, sticky='nesw')
+        row+=1
--- Utils/dispatcher.py
+++ Utils/dispatcher.py
@@ -2,63 +2,84 @@ from pythonosc.dispatcher import Dispatcher
 from pythonosc.udp_client import SimpleUDPClient
 from pythonosc import osc_server
 import asyncio
-
-ADDRESS_PREFIX = "/EmotiBit/0/"
-
-#We are somehow missing: scr:amp,freq,ris , temp, hr, 
-ADDRESS_POSTFIXES = ("GYRO:X", "GYRO:Y", "GYRO:Z",
-                     "MAG:X", "MAG:Y", "MAG:Z",
-                     "ACC:X", "ACC:Y", "ACC:Z",
-                     "PPG:IR", "PPG:RED", "PPG:GRN",
-                     "EDA")
+import numpy as np
+import time
 
 class OSCRelay():
 
+    ADDRESS_PREFIX = "/EmotiBit/0/"
+
+    #We are somehow missing: scr:amp,freq,ris , temp, hr, 
+    ADDRESS_POSTFIXES = ("EDA",
+                        "PPG:IR", "PPG:RED", "PPG:GRN"
+                        "ACC:X", "ACC:Y", "ACC:Z",
+                        "GYRO:X", "GYRO:Y", "GYRO:Z",
+                        "MAG:X", "MAG:Y", "MAG:Z")
+
+    @classmethod
+    def get_presets(_cls):
+        return RelayClient.get_presets()
+    
     def __init__(self):
         self.input = None
         self.clients = []
+        self.vector = np.ones(len(OSCRelay.ADDRESS_POSTFIXES))
         self.stop = False
 
-    def add_client(self, ip, port):
-        self.clients.append(RelayClient(ip, port))
+    def add_client(self, ip, port, preset):
+        self.clients.append(RelayClient(ip, port, preset))
 
     def remove_client(self, index):
         self.clients.pop(index)
 
     def get_clients(self):
-        clients = []
+        clientlist = []
         for client in self.clients:
-            clients.append((client.ip, client.port))
-        return clients
+            clientlist.append((client.ip, client.port, client.preset))
+        return clientlist
     
     async def stop_dispatch(self):
         self.stop = True
+    
+    def msg_handler(self, address, args, index):
+        self.vector[index] = np.mean(args[0]) 
 
-    def msg_handler(self, address, *args):
-        for client in self.clients:
-            client.send_message(address, args)
+    async def dispatch(self, port=12345, frequency=10, ip='127.0.0.1'):
 
-    async def dispatch(self, port=12345):
-
-        self.stop = False
-        
-        ip = '127.0.0.1'
+        self.stop = False       
         dispatcher = Dispatcher()
-        for i, postfix in enumerate(ADDRESS_POSTFIXES):
-            dispatcher.map(ADDRESS_PREFIX + postfix, lambda address, *args: self.msg_handler(address, *args))
+
+        for i, postfix in enumerate(OSCRelay.ADDRESS_POSTFIXES):
+            dispatcher.map(OSCRelay.ADDRESS_PREFIX + postfix, lambda address, *args, index=i: self.msg_handler(address, args, index))
+
         server = osc_server.AsyncIOOSCUDPServer((ip, port), dispatcher, asyncio.get_event_loop())
         transport, protocol = await server.create_serve_endpoint()  # Create datagram endpoint and start serving
-        await self.loop()
+
+        await self.loop(frequency)
         transport.close()
 
-    async def loop(self):
+    async def loop(self, frequency):
+        rest = 1/frequency
         while not self.stop:
-            await asyncio.sleep(0.1)
+            start = time.time()
+            for client in self.clients:
+                client.send_message(OSCRelay.ADDRESS_PREFIX, self.vector[client.mask])
+            await asyncio.sleep(max(0, rest-time.time()+start))
 
 class RelayClient(SimpleUDPClient):
 
-    def __init__(self, ip, port):
+    MASKS = {'ALL': np.array([True, True, True, True, True, True, True, True, True, True, True, True, True]),
+             'IMU': np.array([False, False, False, False, True, True, True, True, True, True, True, True, True]),
+             'BIO': np.array([True, True, True, True, False, False, False, False, False, False, False, False, False, False])}
+
+    def __init__(self, ip, port, preset):
 
         super().__init__(ip, port)
         self.ip = ip
         self.port = port
+        self.preset = preset
+        self.mask = RelayClient.MASKS[preset]
+
+    @classmethod
+    def get_presets(_cls):
+        return [str(key) for key in RelayClient.MASKS.keys()]
